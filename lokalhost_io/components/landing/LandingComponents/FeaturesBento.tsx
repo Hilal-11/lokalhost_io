b5d2dc665f301949a3a63e2ab7f5d97fae896fb4
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-02-17 14:40:43 +0530

    Enhance features bento section

--- lokalhost_io/components/landing/LandingComponents/FeaturesBento.tsx
+++ lokalhost_io/components/landing/LandingComponents/FeaturesBento.tsx
@@ -11,12 +11,17 @@ export default function FeaturesBento() {
             initial={{ opacity: 0, y: 110 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, }}
-            className='z-20  text-center font-sans font-bold text-2xl lg:text-4xl text-neutral-800 dark:text-neutral-200 pb-3'>All Essential Features in one place</motion.h1>
+            className='z-20 text-center font-sans font-bold text-2xl lg:text-4xl text-neutral-800 dark:text-neutral-200 pb-3 tracking-tight'>
+              All Essential Features in{" "}
+              <span className="font-mono text-neutral-500 dark:text-neutral-500">one place</span>
+            </motion.h1>
             <motion.p
             initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, }}
-            className='px-8 lg:px-2 z-20 text-center text-sm lg:text-[17px] font-sans font-medium text-neutral-700 dark:text-neutral-300 lg:w-1/2 mx-auto'>Features the lokalhost.io provides.</motion.p>
+            className='px-8 lg:px-2 z-20 text-center text-sm lg:text-[17px] font-sans font-medium text-neutral-500 dark:text-neutral-400 lg:w-1/2 mx-auto leading-relaxed'>
+              Features that lokalhost.io provides.
+            </motion.p>
         </div>
         <div className="relative h-auto w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-0 lg:gap-0 border-b border-dashed px-px">
           
