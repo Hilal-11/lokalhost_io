3e8ab4595bc45d247971b05ccc4b0fee1eea6983
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-02-24 07:01:20 +0530

    Improve designs page layout and logic

--- app/designs/page.tsx
+++ app/designs/page.tsx
@@ -20,7 +20,10 @@ import {
 } from "@/components/ui/dialog"
 import { RiCheckboxBlankCircleLine } from "react-icons/ri";
 import { SortDesigns } from "./SortDesigns";
-
+import { AnimatedGridPatternDemo } from "./AnimatedGridPattern";
+import AnnoncementBadge from "@/components/landing/AnnoncementBadge";
+import { IoSearchSharp } from "react-icons/io5";
+import { LuFigma } from "react-icons/lu";
 
 function DegignKits(){
     const convex = useConvex();
@@ -255,26 +258,6 @@ export default DegignKits;
 
 
 
-import { cn } from "@/lib/utils"
-import { AnimatedGridPattern } from "@/components/ui/animated-grid-pattern";
-import AnnoncementBadge from "@/components/landing/AnnoncementBadge";
-import { LuFigma } from "react-icons/lu";
-import { IoSearchSharp } from "react-icons/io5";
-export function AnimatedGridPatternDemo() {
-  return (
-    <div className="pointer-events-none absolute inset-0 z-0 mask-x-from-80% mask-y-from-98% dark:mask-from-50% dark:mask-from-98% to-100%">
-    <AnimatedGridPattern
-      numSquares={50}
-      maxOpacity={0.1}
-      duration={3}
-      repeatDelay={1}
-      className={cn(
-        "absolute inset-0",
-        "inset-y-[-30%] h-[1224px]"
-      )}
-    />
-  </div>
-  )
-}
+
 
 
