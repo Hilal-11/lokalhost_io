4f8ebf09101c0fb956c9e42d5b18369b7f857287
Hilal-11 <hilalahmadcodedev123@gmail.com>
2026-02-28 10:50:11 +0530

    done main branch

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,7 @@ import { FinalMainFAQ } from "@/components/FAQ/FAQ";
 export default async function Home() {
     return (
         <main className="bg-white dark:bg-black/5 overflow-x-hidden">
-            <div className="grid grid-rows-[auto_1fr_auto] min-h-screen px-0 lg:px-4 gap-4 sm:gap-12">
+            <div className="grid grid-rows-[auto_1fr_auto] px-0 lg:px-4 gap-4 sm:gap-12">
                 <HeroSection/>
             </div>
             <div className="w-full container max-w-[1580px]">
